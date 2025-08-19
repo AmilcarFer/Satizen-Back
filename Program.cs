33501c7b4beb568a25c97bb8b9d7b01d272a86a7
Fernando <Fernando@DESKTOP-NE3EAQ5>
2025-08-18 22:23:10 -0300

    Se agregó la autenticación para swagger y se quitó el appsettings.json del seguimiento de git

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
-
-using System.Text;
-
-using Satizen_Api.Custom;
-using Satizen_Api.Models;
-using Satizen_Api.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Proyec_Satizen_Api;
 using Satizen_Api;
+using Satizen_Api.Custom;
+using Satizen_Api.Data;
 using Satizen_Api.Hubs;
+using Satizen_Api.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -48,7 +47,31 @@ builder.Services.AddAuthentication(config =>
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 //Acá se agrega el contexto de la base de datos y se define el nombre de la cadena de conexion
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
